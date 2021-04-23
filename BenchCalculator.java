b3cc59a3ac7ac9e24219273fef010785de6bd380
Bmeshanko <bmeshank@purdue.edu>
2021-04-23 15:59:37 -0400

    Modified BenchCalculator to be OOP.

--- BenchCalculator.java
+++ BenchCalculator.java
@@ -1,27 +1,23 @@
-import java.util.Scanner;
-public class BenchCalculator {	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+public class BenchCalculator {
 
-		System.out.println("Enter your One-Rep Max: ");
-		int max = scan.nextInt();
-		
-		int[] sets = new int[7];
+	private int[7][2] sets;
+	private int max;
 
-		sets[0] = (int)(max * 0.55);
-		sets[1] = (int)(max * 0.65);
-		sets[2] = (int)(max * 0.75);
-		sets[3] = (int)(max * 0.85);
-		sets[4] = (int)(max * 0.885);
-		sets[5] = (int)(max * 0.925);
-		sets[6] = (int)(max * 0.75);
+	public BenchCalculator(int max) {
+		sets[0][0] = (int)(max * 0.55);
+		sets[1][0] = (int)(max * 0.65);
+		sets[2][0] = (int)(max * 0.75);
+		sets[3][0] = (int)(max * 0.85);
+		sets[4][0] = (int)(max * 0.885);
+		sets[5][0] = (int)(max * 0.925);
+		sets[6][0] = (int)(max * 0.75);
 
-		System.out.println("---------------------------");
-		System.out.println("Set 1: " + sets[0] + " x 10");
-		System.out.println("Set 2: " + sets[1] + " x 8");
-		System.out.println("Set 3: " + sets[2] + " x 6");
-		System.out.println("Set 4: " + sets[3] + " x 4");
-		System.out.println("Set 5: " + sets[4] + " x 3");
-		System.out.println("Set 6: " + sets[5] + " x 2");
+		sets[0][1] = 10;
+		sets[1][1] = 8;
+		sets[2][1] = 6;
+		sets[3][1] = 4;
+		sets[4][1] = 3;
+		sets[5][1] = 2;
+		sets[6][1] = 6;
 	}
 }
