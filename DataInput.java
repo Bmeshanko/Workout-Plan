95a82ac524b2f48a8f3f2232c6f6f7886ede3e85
Bmeshanko <bmeshank@purdue.edu>
2021-04-25 19:34:03 -0400

    writeFile method

--- DataInput.java
+++ DataInput.java
@@ -1,7 +1,21 @@
 import java.io.*;
 
 public class DataInput {
+
+	private int benchMax;
+	private int squatMax;
+	private int deadliftMax;
+
+
 	public void readFile(String fileName) {
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+		
+	}
+
+	public void writeFile(String fileName) {
+		PrintWriter pw = new PrintWriter(fileName);
+	
+
 	}
 }
