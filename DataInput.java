add8b40c71d33f5ed360b3b4894053c12d2b7b06
Bmeshanko <bmeshank@purdue.edu>
2021-04-27 19:32:01 -0400

    Modified input.txt

--- DataInput.java
+++ DataInput.java
@@ -2,40 +2,45 @@ import java.io.*;
 
 public class DataInput {
 
-	private int[] pushMaxes;
-	// 0: Bench Press
-	// 1: Incline Bench Press;
+	
+	// Machines with Star will be max weight.
+	// Machines without Star will be 4x10 weight.
+
+	private int[] pushLifts;
+	// 0: Bench Press*
+	// 1: Incline Bench Press*
 	// 2: Deltoid Flex
 	// 3: Tricep Extension
 	// 4: Tricep Dips
 	// 5: Tricep Pushdown
 	// 6: Shoulder Press
 	// 7: Lateral/Front Raises
-	// 8: Clean and Press
+	// 8: Clean and Press*
 	// Not Included: Push-ups
 	
-	private int[] pullMaxes;
+	private int[] pullLifts;
 	// 0: LAT Pulldown
-	// 1: Deadlift
+	// 1: Deadlift*
 	// 2: Low Row
 	// 3: Seated Bicep Curls
 	// 4: Dumbbell Row
 	// 5: Standing Cable Curls
 	// Not Included: Pull Ups
 
-	private int[] legMaxes;
-	// 0: Squat
-	// 1: Lunge
-	// 2: Leg Press
+	private int[] legLifts;
+	// 0: Squat*
+	// 1: Lunge*
+	// 2: Leg Press*
 	// 3: Leg Curl
 	// 4: Leg Extension
 	// 5: Inner Thigh
 	// 6: Outer Thigh
 	// Not Included: Calf Raises
 
-	public void readFile(String fileName) {
-		BufferedReader br = new BufferedReader(new FileReader(fileName));
-
+	public void readFile() {
+		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+		
+		String
 		
 	}
 
