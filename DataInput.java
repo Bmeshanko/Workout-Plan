f8c5b0df22d672157853eafeec5ef03e38de3139
Bmeshanko <bmeshank@purdue.edu>
2021-04-27 22:07:35 -0400

    Changes to the lifts array.

--- DataInput.java
+++ DataInput.java
@@ -5,7 +5,7 @@ public class DataInput {
 	// Machines with Star will be max weight.
 	// Machines without Star will be 4x10 weight.
 
-	private int[] pushLifts;
+	private int[] lifts;
 	// 0: Bench Press*
 	// 1: Incline Bench Press*
 	// 2: Deltoid Flex
@@ -15,68 +15,52 @@ public class DataInput {
 	// 6: Shoulder Press
 	// 7: Lateral/Front Raises
 	// 8: Clean and Press*
-	// Not Included: Push-ups
-	
-	private int[] pullLifts;
-	// 0: LAT Pulldown
-	// 1: Deadlift*
-	// 2: Low Row
-	// 3: Seated Bicep Curls
-	// 4: Dumbbell Row
-	// 5: Standing Cable Curls
-	// Not Included: Pull Ups
+	// 9: LAT Pulldown
+	// 10: Deadlift*
+	// 11: Low Row
+	// 12: Seated Bicep Curls
+	// 13: Dumbbell Row
+	// 14: Standing Cable Curls
+	// 15: Squat*
+	// 16: Lunge*
+	// 17: Leg Press*
+	// 18: Leg Curl
+	// 19: Leg Extension
+	// 20: Inner Thigh
+	// 21: Outer Thigh
 
-	private int[] legLifts;
-	// 0: Squat*
-	// 1: Lunge*
-	// 2: Leg Press*
-	// 3: Leg Curl
-	// 4: Leg Extension
-	// 5: Inner Thigh
-	// 6: Outer Thigh
-	// Not Included: Calf Raises
+	public int[] getLifts() {
+		return this.lifts;
+	}
 
 	public void readInputFile() throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 		
-		int c = 0;
-		int[] allLifts = new int[22];
+		int i = 0;
+		this.lifts = new int[22];
 		while(true) {
 			String line = br.readLine();
 			if (line == null) break;
-			allLifts[c++] = Integer.parseInt(line);
-		}
-
-		for (int i = 0; i < 9; i++) {
-			this.pushLifts[i] = allLifts[i];
-		}
-
-		for (int i = 0; i < 6; i++) {
-			this.pullLifts[i] = allLifts[i + 9];
-		}
-
-		for (int i = 0; i < 7; i++) {
-			this.legLifts[i] = allLifts[i + 15];
+			allLifts[i++] = Integer.parseInt(line);
 		}
 	}
 
 	public void writeInputFile() throws IOException {
 		PrintWriter pw = new PrintWriter("input.txt");
 		
-		for (int i = 0; i < 9; i++) {
-			pw.println(this.pushLifts[i]);
-		}
-
-		for (int i = 0; i < 6; i++) {
-			pw.println(this.pullLifts[i]);
-		}
-
-		for (int i = 0; i < 7; i++) {
-			pw.println(this.legLifts[i]);
+		for (int i = 0; i < 22; i++) {
+			
 		}
 	}
 
 	public void writeOutputFile() throws IOException {
 
+	}
+
+	public static void main(String[] args) {
+		DataInput data = new DataInput();
+		data.readInputFile();
+
+		Calculator calc = new Calculator();
 	}
 }
